b4e5da5938c1d6e7c1411b6dd49ee10e7b24764a
Laxman <laxman0202@gmail.com>
2025-03-14 10:46:17 -0400

    Add Flathub repo

--- build.sh
+++ build.sh
@@ -23,4 +23,8 @@ set -ouex pipefail
 
 # systemctl enable podman.socket
 
+# Add Flathub by default
+mkdir -p /etc/flatpak/remotes.d
+curl --retry 3 -o /etc/flatpak/remotes.d/flathub.flatpakrepo "https://dl.flathub.org/repo/flathub.flatpakrepo"
+
 dconf update
